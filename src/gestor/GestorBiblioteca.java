6042168970fb7593cfa1401437e77c8208a6a107
Irene Herrero <ireneherrero@outlook.com>
2023-01-26 12:14:11 +0100

    comit base

---
+++ src/gestor/GestorBiblioteca.java
@@ -0,0 +1,12 @@
+package gestor;
+
+import principal.BibliotecaApp;
+
+public class GestorBiblioteca {
+
+	public static void main(String[] args) {
+		BibliotecaApp.run();
+
+	}
+
+}
---
+++ src/principal/BibliotecaApp.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class BibliotecaApp {
+
+	public static void run() {
+		
+	}
+	
+}
