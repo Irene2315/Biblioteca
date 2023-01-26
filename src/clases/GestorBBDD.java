78e6fe1e7e61b5ad0daea63aa150a623669dbced
Irene Herrero <ireneherrero@outlook.com>
2023-01-26 13:33:01 +0100

    subir BDD

---
+++ src/clases/GestorBBDD.java
@@ -0,0 +1,32 @@
+package clases;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GestorBBDD extends Conector  {
+	PreparedStatement preparedSt;
+	
+	public void insertarLibro(Libro libro) throws SQLException {
+		
+		preparedSt= con.prepareStatement("INSERT INTO `libros` "
+				+ "(`id`, `titulo`, `autor`, `num_pag`) "
+				+ "VALUES (?,?,?,?)");
+		preparedSt.setInt(1, libro.getId());
+		preparedSt.setString(2, libro.getTitulo());
+		preparedSt.setString(3, libro.getAutor());
+		preparedSt.setInt(3, libro.getNum_pag());
+		
+		preparedSt.execute();
+		
+	}
+	
+	public void eliminarLibro(int id) throws SQLException {
+		
+		preparedSt= con.prepareStatement("DELETE FROM libros WHERE `libros`.`id` = ?");
+		
+	}
+	public Libro getLibro(int id) {
+		
+	}
+	
+}
