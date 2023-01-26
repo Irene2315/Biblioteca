9753fe83a5599eb9201d35cf365725f17d43cf85
Irene Herrero <ireneherrero@outlook.com>
2023-01-26 12:33:29 +0100

    menu creado

--- src/clases/Menu.java
+++ src/clases/Menu.java
@@ -1,5 +1,37 @@
 package clases;
 
 public class Menu {
-
+	
+	public static void mostrarMenuPrincipal() {
+		System.out.println("MENU PRINCIPAL");
+		System.out.println("--------------");
+		System.out.println("1. Gestionar Libros");
+		System.out.println("2. Gestionar Socios");
+		System.out.println("3. Gestionar Prestamos");
+		System.out.println("");
+	}
+	
+	public static void mostrarMenuLibros() {
+		System.out.println("MENU LIBROS");
+		System.out.println("------------");
+		System.out.println("1. Isertar libro");
+		System.out.println("2. Eliminar libro");
+		System.out.println("3. Ver libros");
+		System.out.println("");
+		
+	}
+	
+	public static void mostrarMenuSocios() {
+		System.out.println("MENU SOCIOS");
+		System.out.println("-----------");
+		System.out.println("1. Ver socios");
+		System.out.println("");
+	}
+	
+	public static void mostrarMenuPrestamos() {
+		System.out.println("MENU PRESTAMOS");
+		System.out.println("--------------");
+		System.out.println("1. Realizar prestamo");
+		System.out.println("");
+	}
 }
--- src/principal/BibliotecaApp.java
+++ src/principal/BibliotecaApp.java
@@ -1,9 +1,14 @@
 package principal;
 
+import clases.Menu;
+
 public class BibliotecaApp {
 
 	public static void run() {
-		
+		Menu.mostrarMenuPrincipal();
+		Menu.mostrarMenuLibros();
+		Menu.mostrarMenuSocios();
+		Menu.mostrarMenuPrestamos();
 	}
 	
 }
