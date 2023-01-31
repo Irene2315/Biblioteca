5d8ef6d37521272f594ee983e9165ce6db927ca5
Irene Herrero <ireneherrero@outlook.com>
2023-01-31 09:22:45 +0100

    empezamos función principal

--- src/gestor/GestorBiblioteca.java
+++ src/gestor/GestorBiblioteca.java
@@ -1,12 +1,45 @@
 package gestor;
 
-import principal.BibliotecaApp;
+import java.util.Scanner;
+
+import clases.Menu;
 
 public class GestorBiblioteca {
+	
+	static Scanner scan = new Scanner (System.in);
 
-	public static void main(String[] args) {
-		BibliotecaApp.run();
+	public static void run() {
+		int opcion;
 
+		do {
+			Menu.mostrarMenuPrincipal();
+			
+			opcion= scan.nextInt();
+
+			switch (opcion) {
+			case Menu.GESTIONAR_LIBROS:
+				System.out.println("Primera opcion selecionada");
+
+				break;
+			case OPCION_DOS:
+				System.out.println("Segunda opcion selecionada");
+				break;
+			case OPCION_TRES:
+				System.out.println("Tercera opcion selecinada ");
+				break;
+			case OPCION_CUATRO:
+
+				System.out.println("Cuarta opcion selecionada");
+				break;
+			case SALIR:
+
+			}
+			System.out.println("");
+		} while (opcion_menu != SALIR);
+		teclado.close();
+		
 	}
+
+	
 
 }
---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import gestor.GestorBiblioteca;
+
+public class Main {
+
+	public static void main (String[] args) {
+		GestorBiblioteca.run();
+
+	}
+
+}
